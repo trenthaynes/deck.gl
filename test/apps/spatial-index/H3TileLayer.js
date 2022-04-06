afaf1df7f8e00c6ae8b918e76bed6133f36f23ea
Felix Palmer <felixpalmer@gmail.com>
2022-04-06 12:24:12 +0200

    Skeleton for H3TileLayer

---
+++ test/apps/spatial-index/H3TileLayer.js
@@ -0,0 +1,7 @@
+import {TileLayer} from '@deck.gl/geo-layers';
+
+const defaultProps = {};
+
+export default class H3TileLayer extends TileLayer {}
+H3TileLayer.layerName = 'H3TileLayer';
+H3TileLayer.defaultProps = defaultProps;
