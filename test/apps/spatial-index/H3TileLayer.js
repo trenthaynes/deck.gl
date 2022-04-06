066422139e1ff4218890c6f75f2ba5af19c6f65b
Felix Palmer <felixpalmer@gmail.com>
2022-04-06 12:53:03 +0200

    Copy in tileset code

--- test/apps/spatial-index/H3TileLayer.js
+++ test/apps/spatial-index/H3TileLayer.js
@@ -1,7 +1,19 @@
+import H3Tileset2D from './h3-tileset-2d';
 import {TileLayer} from '@deck.gl/geo-layers';
 
 const defaultProps = {};
 
-export default class H3TileLayer extends TileLayer {}
+export default class H3TileLayer extends TileLayer {
+  // Duplicate implementation from TileLayer so we can change Tileset2D
+  updateState({props, changeFlags}) {
+    let {tileset} = this.state;
+    if (!tileset) {
+      tileset = new H3Tileset2D(this._getTilesetOptions(props));
+      this.setState({tileset});
+    }
+
+    super.updateState({props, changeFlags});
+  }
+}
 H3TileLayer.layerName = 'H3TileLayer';
 H3TileLayer.defaultProps = defaultProps;
