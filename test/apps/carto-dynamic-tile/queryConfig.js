2491b034bc2322d616c32c68c5b301d0ae0fece1
Alberto Asuero <albertoasuero@gmail.com>
2022-04-26 12:27:01 +0200

    New point tables

--- test/apps/carto-dynamic-tile/queryConfig.js
+++ test/apps/carto-dynamic-tile/queryConfig.js
@@ -1,9 +1,46 @@
 export default {
   bigquery: {
+    points_10k: `select * from carto-dev-data.public.points_10k`,
     points_1m: `select * from carto-dev-data.public.points_1m`,
     points_5m: `select * from carto-dev-data.public.points_5m`,
+    points_10m: `select * from carto-dev-data.public.points_10m`,
     censustract: `select * from carto-dev-data.public.geography_usa_censustract_2019`,
+    county: 'select * carto-dev-data.public.geography_usa_county_2019',
     blockgroup: `select * from carto-dev-data.public.geography_usa_blockgroup_2019`,
-    zipcodes: `select * from carto-dev-data.public.geography_usa_zcta5_2019`
+    zipcodes: `select * from carto-dev-data.public.geography_usa_zcta5_2019`,
+    block: 'select * from carto-dev-data.public.geography_usa_block_2019'
+  },
+  snowflake: {
+    points_10k: `select * from carto_dev_data.public.points_10k`,
+    points_1m: `select * from carto_dev_data.public.points_1m`,
+    points_5m: `select * from carto_dev_data.public.points_5m`,
+    points_10m: `select * from carto_dev_data.public.points_10m`,
+    censustract: `select * from carto_dev_data.public.geography_usa_censustract_2019`,
+    county: 'select * from carto_dev_data.public.geography_usa_county_2019',
+    blockgroup: `select * from carto_dev_data.public.geography_usa_blockgroup_2019`,
+    zipcodes: `select * from carto_dev_data.public.geography_usa_zcta5_2019`,
+    block: 'select * from carto_dev_data.public.geography_usa_block_2019'
+  },
+  redshift: {
+    points_10k: `select * from public.points_10k`,
+    points_1m: `select * from public.points_1m`,
+    points_5m: `select * from public.points_5m`,
+    points_10m: `select * from public.points_10m`,
+    censustract: `select * from public.geography_usa_censustract_2019`,
+    county: 'select * from public.geography_usa_county_2019',
+    blockgroup: `select * from public.geography_usa_blockgroup_2019`,
+    zipcodes: `select * from public.geography_usa_zcta5_2019`,
+    block: 'select * from public.geography_usa_block_2019'
+  },
+  postgres: {
+    points_10k: `select * from public.points_10k`,
+    points_1m: `select * from public.points_1m`,
+    points_5m: `select * from public.points_5m`,
+    points_10m: `select * from public.points_10m`,
+    censustract: `select * from public.geography_usa_censustract_2019`,
+    county: 'select * from public.geography_usa_county_2019',
+    blockgroup: `select * from public.geography_usa_blockgroup_2019`,
+    zipcodes: `select * from public.geography_usa_zcta5_2019`,
+    block: 'select * from public.geography_usa_block_2019'
   }
 };
--- test/apps/carto-dynamic-tile/tableConfig.js
+++ test/apps/carto-dynamic-tile/tableConfig.js
@@ -1,39 +1,46 @@
 export default {
   bigquery: {
-    points_1m: 'cartodb-gcp-backend-data-team.dynamic_tiling.points_1M',
-    points_5m: 'cartodb-gcp-backend-data-team.dynamic_tiling.points_5M',
-    censustract: 'carto-do-public-data.carto.geography_usa_censustract_2019',
-    blockgroup: 'carto-do-public-data.carto.geography_usa_blockgroup_2019',
-    zipcodes: 'carto-do-public-data.carto.geography_usa_zcta5_2019',
-    h3: 'carto-do-public-data.carto.geography_usa_h3res8_v1',
-    block: 'carto-do-public-data.carto.geography_usa_block_2019'
+    points_10k: `carto-dev-data.public.public.points_10k`,
+    points_1m: 'carto-dev-data.public.points_1m',
+    points_5m: 'carto-dev-data.public.points_5m',
+    points_10m: 'carto-dev-data.public.points_10m',
+    censustract: 'carto-dev-data.public.geography_usa_censustract_2019',
+    county: 'carto-dev-data.public.geography_usa_county_2019',
+    blockgroup: 'carto-dev-data.public.geography_usa_blockgroup_2019',
+    zipcodes: 'carto-dev-data.public.geography_usa_zcta5_2019',
+    block: 'carto-dev-data.public.geography_usa_block_2019'
   },
   snowflake: {
-    points_1m: 'carto_backend_data_team.dynamic_tiling.points_1M',
-    points_5m: 'carto_backend_data_team.dynamic_tiling.points_5M',
-    censustract: 'carto_backend_data_team.dynamic_tiling.usa_censustract_2019',
-    blockgroup: 'carto_backend_data_team.dynamic_tiling.usa_blockgroup_2019',
-    zipcodes: 'carto_backend_data_team.dynamic_tiling.usa_zcta5_2019',
-    h3: 'carto_backend_data_team.dynamic_tiling.usa_h3res8_v1',
-    block: 'carto_backend_data_team.dynamic_tiling.usa_block_2019'
+    points_10k: `carto_dev_data.public.public.points_10k`,
+    points_1m: 'carto_dev_data.public.points_1m',
+    points_5m: 'carto_dev_data.public.points_5m',
+    points_10m: 'carto_dev_data.public.points_10m',
+    censustract: 'carto_dev_data.public.geography_usa_censustract_2019',
+    county: 'carto_dev_data.public.geography_usa_county_2019',
+    blockgroup: 'carto_dev_data.public.geography_usa_blockgroup_2019',
+    zipcodes: 'carto_dev_data.public.geography_usa_zcta5_2019',
+    block: 'carto_dev_data.public.geography_usa_block_2019'
   },
   redshift: {
-    points_1m: 'carto_backend_data_team.dynamic_tiling.points_1m',
-    points_5m: 'carto_backend_data_team.dynamic_tiling.points_5m',
-    censustract: 'carto_backend_data_team.dynamic_tiling.usa_censustract_2019',
-    blockgroup: 'carto_backend_data_team.dynamic_tiling.usa_blockgroup_2019',
-    zipcodes: 'carto_backend_data_team.dynamic_tiling.usa_zcta5_2019',
-    h3: 'carto_backend_data_team.dynamic_tiling.usa_h3res8_v1',
-    block: 'carto_backend_data_team.dynamic_tiling.usa_block_2019'
+    points_10k: `public.public.points_10k`,
+    points_1m: 'public.points_1m',
+    points_5m: 'public.points_5m',
+    points_10m: 'public.points_10m',
+    censustract: 'public.geography_usa_censustract_2019',
+    county: 'public.geography_usa_county_2019',
+    blockgroup: 'public.geography_usa_blockgroup_2019',
+    zipcodes: 'public.geography_usa_zcta5_2019',
+    block: 'public.geography_usa_block_2019'
   },
   postgres: {
-    points_1m: 'demo.demo_tables.points_1m',
-    points_5m: 'demo.demo_tables.points_5m',
-    points_10m: 'demo.demo_tables.points_10m',
-    censustract: 'demo.demo_tables.usa_censustract_2019',
-    blockgroup: 'demo.demo_tables.usa_blockgroup_2019',
-    zipcodes: 'demo.demo_tables.usa_zcta5_2019',
-    county: 'demo.demo_tables.usa_county_2019',
-    block: 'demo.demo_tables.usa_block_2019'
+    points_10k: `public.public.points_10k`,
+    points_1m: 'public.points_1m',
+    points_5m: 'public.points_5m',
+    points_10m: 'public.points_10m',
+    censustract: 'public.geography_usa_censustract_2019',
+    county: 'public.geography_usa_county_2019',
+    blockgroup: 'public.geography_usa_blockgroup_2019',
+    zipcodes: 'public.geography_usa_zcta5_2019',
+    block: 'public.geography_usa_block_2019'
   }
 };
